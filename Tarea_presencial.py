b5fb8a23dc8898af8893aa6187af9d9ffc586c11
rsimbrelo <rsimbrelo@gmail.com>
2024-09-11 11:00:15 -0300

    Update ejercicio 1

--- Tarea_presencial.py
+++ Tarea_presencial.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def Ejercicio1():
     datos = request.get_json()
 
+    #Hay que pasarle un json con el campo cadena y el string a procesar en el valor
     if 'cadena' not in datos:
         return jsonify({'error': 'Falta el parámetro "cadena".'}), 400
 
     cadena = datos['cadena']
 
-    cadena_procesada = False
     cadena_procesada = (cadena == cadena[::-1])
     return jsonify(cadena_procesada)
 
